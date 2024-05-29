f2e68f2d79ee05dc4311b15c4a65288e0afc3c09
iamfyke <fyketonel@gmail.com>
2024-05-29 23:09:58 +0800

    main activity 2 default font and status bar transparency

--- app/src/main/java/com/example/appointmentscheduler/MainActivity2.java
+++ app/src/main/java/com/example/appointmentscheduler/MainActivity2.java
@@ -1,9 +1,15 @@
 package com.example.appointmentscheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
+
+import java.lang.reflect.Field;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -12,9 +18,26 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        ConstraintLayout main_layout2 = findViewById(R.id.main_layout2);
+        main_layout2.setBackgroundColor(Color.parseColor("#F8F9FA"));
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
 
+        setDefaultFont();
 
+    }
 
+    private void setDefaultFont() {
+        try {
+            final Typeface inter = Typeface.createFromAsset(getAssets(), "fonts/inter_regular.ttf");
+            final Field defaultFontTypefaceField = Typeface.class.getDeclaredField("SERIF");
+            defaultFontTypefaceField.setAccessible(true);
+            defaultFontTypefaceField.set(null, inter);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
