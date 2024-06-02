ab2e371aa59532f11f5cb92094d02407ced0b3fb
iamfyke <fyketonel@gmail.com>
2024-06-02 21:40:56 +0800

    navabr button functions

--- app/src/main/java/com/example/appointmentscheduler/MainActivity4.java
+++ app/src/main/java/com/example/appointmentscheduler/MainActivity4.java
@@ -4,7 +4,11 @@ package com.example.appointmentscheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity4 extends AppCompatActivity {
 
@@ -12,5 +16,45 @@ public class MainActivity4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+        ImageButton homeButton = findViewById(R.id.homeButton);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity4.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton addButton = findViewById(R.id.addButton);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity4.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton profileButton = findViewById(R.id.profileButton);
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity4.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton menuButton = findViewById(R.id.menuButton);
+
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity4.this, MainActivity5.class);
+                startActivity(intent);
+            }
+        });
     }
 }
