05031731e907da62ed6d55b8fcc7d07a81acc0cf
iamfyke <fyketonel@gmail.com>
2024-06-06 18:52:55 +0800

    animation slide when switching intents

--- app/src/main/java/com/example/appointmentscheduler/MainActivity.java
+++ app/src/main/java/com/example/appointmentscheduler/MainActivity.java
@@ -58,6 +58,7 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, MainActivity4.class);
                 startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             }
         });
 
